1bcb6877e35bc84bea53e5a9a56d2c6bc10c35cd
Fahim <fahim.madani@gmail.com>
2022-05-26 14:59:29 +0600

    admin create successful

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import { auth } from '../../firebase.init';
+import useAdmin from '../../useAdmin';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth)
+    const [admin] = useAdmin(user)
     return (
         <div class="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
@@ -15,10 +20,13 @@ const Dashboard = () => {
                 <label for="my-drawer-2" class="drawer-overlay"></label>
                 <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
-                    <li><Link to="/dashboard/myorders">My Orders</Link></li>
-                    <li><Link to="/dashboard/addreview">Add a Review</Link></li>
+                    {!admin && <li><Link to="/dashboard/myorders">My Orders</Link></li>}
+                    {!admin && <li><Link to="/dashboard/addreview">Add a Review</Link></li>}
                     <li><Link to="/dashboard/profile">My Profile</Link></li>
-                    <li><Link to="/dashboard/users">All Users</Link></li>
+                    {admin && <li><Link to="/dashboard/users">All Users</Link></li>}
+                    {admin && <li><Link to="/dashboard/users">Manage</Link></li>}
+                    {admin && <li><Link to="/dashboard/users">Add Products</Link></li>}
+                    {admin && <li><Link to="/dashboard/users">All Orders</Link></li>}
                 </ul>
 
             </div>
--- src/Pages/Dashboard/UserRows.js
+++ src/Pages/Dashboard/UserRows.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import toast from 'react-hot-toast';
 
-const UserRows = ({user, index, refetch}) => {
-    const {email, role} = user
+const UserRows = ({ user, index, refetch }) => {
+    const { email, role } = user
     const makeAdmin = () => {
-        fetch(`http://localhost:5000/user/admin/${email}`,{
-            method : 'PUT',
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
         })
-        .then(res => res.json())
-        .then(data => {
-            refetch()
-            toast.success('Admin Created')
-        })
+            .then(res => {
+                if(res.status === 403){
+                    toast.error('failed to make Admin', {id: 'anything'});
+                }
+            return res.json()
+            })
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    refetch()
+                    toast.success('Admin Created', {id : 'sth'})
+                }
+                
+
+            })
     }
     return (
-        
-            <tr>
-                <th>{index + 1}</th>
-                <td>{email}</td>
-                
-                <td>{role !== "admin" && <button class="btn btn-sm" onClick={makeAdmin}>Make Admin</button>}</td>
-                <td><button class="btn btn-sm">Remove User</button></td>
-            </tr>
-        
+
+        <tr>
+            <th>{index + 1}</th>
+            <td>{email}</td>
+
+            <td>{role !== "admin" && <button class="btn btn-sm" onClick={makeAdmin}>Make Admin</button>}</td>
+            <td><button class="btn btn-sm">Remove User</button></td>
+        </tr>
+
     );
 };
 
